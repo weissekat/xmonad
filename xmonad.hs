2e62b309ce69cb663d8d7871e214ccfdadc2bd15
weissekat <weissekat@gmail.com>
2015-04-28 22:30:46 +0300

    Tune and colorize borders, add few comments

--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,17 @@
 import XMonad
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig(additionalKeys,removeKeys)
 
 main = do
-	xmonad $ defaultConfig 
-		{ 
+	xmonad $ defaultConfig { 
+			-- WIN-key as modkey --
 			modMask = mod4Mask
-		} `additionalKeys` 
+			-- tune and colorize borders --
+			,borderWidth           = 3
+			,normalBorderColor  = "grey"
+			,focusedBorderColor  = "green"
+		}
+		`additionalKeys`
 		[
 			-- change keyboard layout --
 			((controlMask, xK_Shift_L), spawn "~/.xmonad/keyboard-switch.sh") 
