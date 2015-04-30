14a4b76132ea5321444b11605bed8517bbbe0204
weissekat <weissekat@gmail.com>
2015-04-30 16:03:28 +0300

    Volume indicator message changed.

--- getvolume.sh
+++ getvolume.sh
@@ -3,5 +3,5 @@ if [ $(pacmd list-sinks | grep "muted:" | awk '{print $2}') = "no" ]; then
 	vol="$(pacmd list-sinks | grep "volume: 0:"| awk '{print $3}')"
 	echo "vol: $vol"
 else
-	echo "mute!"
+	echo "mute"
 fi
