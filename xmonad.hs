022fa9eac51e150985ade9732e4ef4fb9d400800
Volodymyr Borysenko <Volodymyr.Borysenko@portaone.com>
2016-03-01 10:18:36 +0200

    Work apps startup added

--- xmonad.hs
+++ xmonad.hs
@@ -20,7 +20,7 @@ voManageHook = composeAll . concat $
         , [ className =? c --> doShift "3:drei"   | c <- voDreiClass   ]
         , [ className =? c --> doShift "4:vier"   | c <- voVierClass   ]
         , [ className =? c --> doShift "5:fünf"   | c <- voFunfClass   ]
-        , [ className =? c --> doShift "6:sechs"  | c <- vSechsClass   ]
+        , [ className =? c --> doShift "6:sechs"  | c <- voSechsClass  ]
         , [ className =? c --> doShift "7:sieben" | c <- voSiebenClass ]
         , [ className =? c --> doShift "8:acht"   | c <- voAchtClass   ]
         , [ className =? c --> doShift "9:neun"   | c <- voNeunClass   ]
@@ -31,11 +31,20 @@ voManageHook = composeAll . concat $
         voDreiClass   = []
         voVierClass   = []
         voFunfClass   = []
-        vSechsClass   = []
-        voSiebenClass = []
-        voAchtClass   = []
+        voSechsClass  = []
+        voSiebenClass = ["Pidgin"]
+        voAchtClass   = ["Thunderbird"]
         voNeunClass   = []
         voNullClass   = []
+
+voStartupHook :: X ()
+voStartupHook = do
+    -- run xkb config on startup
+    spawn "~/.xmonad/initkey.sh"
+
+    -- run work apps
+    spawn "pidgin"
+    spawn "thunderbird"
 
 -- spawn with balls; warning, you password shows on screen and may be saved if historySize > 0
 xpConfig = defaultXPConfig { font = "-misc-fixed-*-*-*-*-20-*-*-*-*-*-*-*", position = Bottom, historySize = 0 }
@@ -49,48 +58,47 @@ main = do
 
     -- fix for incorrect GTK theme; config: "gtk-theme-config", "xfce4-appearance-settings"
     spawn "xfsettingsd"
-    
-    xmonad $ defaultConfig { 
+
+    xmonad $ defaultConfig {
             -- WIN-key as modkey
             modMask = mod4Mask
-            
+
             -- tune and colorize borders
             , borderWidth = 3
             , normalBorderColor = "gray"
             , focusedBorderColor = "green"
-            
+
             -- define workspaces
             , workspaces = voWorkspaces
-            
+
             -- manage predefined hooks
             , manageHook = voManageHook
-            
+
             -- add xmobar borders that windows SHALL NOT PASS
             , layoutHook = avoidStruts  $  layoutHook defaultConfig
-            
+
             -- add xmobar pipe with workspace/tiling name
             , logHook =  dynamicLogWithPP $ defaultPP {
                     ppOutput = System.IO.hPutStrLn xmproc
-                } 
+                }
 
-            -- run xkb config on startup
-            , startupHook = spawn "~/.xmonad/initkey.sh"
+            , startupHook = voStartupHook
         }
         `additionalKeys`
         [
             -- increase font for "dmenu"; however, dmenu haven't support for lovely antialiased Droid Sans/Monospace
             ((mod4Mask, xK_p), spawn "dmenu_run -fn \"-misc-fixed-*-*-*-*-20-*-*-*-*-*-*-*\"")
-            
+
             {-|
             -- volume control / alsa
             , ((mod4Mask , xK_F7), spawn "amixer set Master off && amixer set Headphone off")
-            , ((mod4Mask , xK_F8), spawn "amixer set Master on && amixer set Headphone on && amixer set Master 2-") 
+            , ((mod4Mask , xK_F8), spawn "amixer set Master on && amixer set Headphone on && amixer set Master 2-")
             , ((mod4Mask , xK_F9), spawn "amixer set Master on && amixer set Headphone on && amixer set Master 2+")
             -}
-            
+
             -- volume control / pulseaudio
             , ((mod4Mask , xK_F7), spawn "pactl set-sink-mute 0 toggle")
-            , ((mod4Mask , xK_F8), spawn ".xmonad/decvolume.sh") 
+            , ((mod4Mask , xK_F8), spawn ".xmonad/decvolume.sh")
             , ((mod4Mask , xK_F9), spawn ".xmonad/incvolume.sh")
 
             -- suspend mode with balls
@@ -103,12 +111,12 @@ main = do
         [
             -- volume control / pulseaudio for notebook
             ("<XF86AudioMute>", spawn "pactl set-sink-mute 0 toggle")
-            , ("<XF86AudioLowerVolume>", spawn ".xmonad/decvolume.sh") 
+            , ("<XF86AudioLowerVolume>", spawn ".xmonad/decvolume.sh")
             , ("<XF86AudioRaiseVolume>", spawn ".xmonad/incvolume.sh")
-            
+
             -- suspend mode with balls for notebook
             , ("<XF86Sleep>", sudoSpawn "pm-suspend")
-            
+
             -- brightness control for notebook
             , ("<XF86MonBrightnessUp>", spawn "xbacklight +10")
             , ("<XF86MonBrightnessDown>", spawn "xbacklight -10")
@@ -116,4 +124,4 @@ main = do
         `removeKeys`
         [
             (controlMask, xK_Shift_L)
-        ]   
+        ]
